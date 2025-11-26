2981aa58e04a6a9f0e6827d938c9712e3dbab38e
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-11-26 19:13:35 +0900

    [add] - adding ui

--- src/pages/main/ui/Main.tsx
+++ src/pages/main/ui/Main.tsx
@@ -1,14 +1,77 @@
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+} from "@/shared/components/ui/sidebar";
 import { Wrapper } from "@/shared/components/ui/wrapper";
 import { Dashboard } from "@/widgets/dashboard/ui/Dashboard";
 import { Header } from "@/widgets/header/ui/Header";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+];
 
 export const Main = () => {
   return (
     <>
-      <Wrapper className="fixed px-4 py-4 justify-end z-5">
+      <SidebarProvider>
+        {/* <Wrapper className="fixed px-4 py-4 justify-end z-5"> */}
+        {/* </Wrapper> */}
+        <Sidebar>
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupLabel>Application</SidebarGroupLabel>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  {items.map((item) => (
+                    <SidebarMenuItem key={item.title}>
+                      <SidebarMenuButton asChild>
+                        <a href={item.url}>
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </a>
+                      </SidebarMenuButton>
+                    </SidebarMenuItem>
+                  ))}
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </Sidebar>
         <Header />
-      </Wrapper>
-      <Dashboard />
+        <Dashboard />
+      </SidebarProvider>
     </>
   );
 };
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,11 +1,10 @@
-import { CircleUserRound } from "lucide-react";
+import { SidebarTrigger } from "@/shared/components/ui/sidebar";
+import { SidebarIcon } from "lucide-react";
 
 export const Header = () => {
   return (
-    <>
-      <header className="flex relative bg-(--background) p-2 shadow-(--drop-shadow) rounded-(--radius) cursor-pointer z-10">
-        <CircleUserRound />
-      </header>
-    </>
+    <SidebarTrigger className="size-12 [&_svg:not([class*='size-'])]:size-6">
+      <SidebarIcon />
+    </SidebarTrigger>
   );
 };
