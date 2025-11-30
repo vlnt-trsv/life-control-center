4ce3eaf156df99499b4a351162981b0f1e75779e
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-11-30 16:32:45 +0900

    [fix] - Изменение маппера

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -1,7 +1,5 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-import type { WidgetRecord, WidgetRow } from "../api/types/types.db";
-import type { WidgetNode } from "@/entities/node/types/types";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -18,31 +16,17 @@ export function debounce<T extends (...args: never[]) => void>(
   }) as T;
 }
 // TODO: Написать типы для маппов
-export function mapToDB(row: any) {
-  return {
-    id: row.id,
-    type: row.type,
-    title: row.title,
-    widget_type: row.widget_type,
-    position_x: row.position_x,
-    position_y: row.position_y,
-    width: row.width,
-    height: row.height,
-    createdAt: row.created_at,
-    updatedAt: row.updated_at,
-  };
-}
-
+// Переписать мапу, сделать универсальную функцию (если есть несколько слов, то перед ними ставить _)
 export function mapToNode(rec: any) {
   return {
     id: rec.id,
     type: rec.type,
     position: {
-      x: rec.position_x,
-      y: rec.position_y,
+      x: rec.positionX,
+      y: rec.positionY,
     },
     data: {
-      widget_type: rec.widget_type?.value,
+      widgetType: rec.widgetType,
       title: rec.title,
     },
   };
