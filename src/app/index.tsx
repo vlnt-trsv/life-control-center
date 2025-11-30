3dee29c05644473f3c5f5550c832940a83c287e7
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-11-30 16:30:55 +0900

    [fix] - Добавлен тостер обёртка

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { ThemeProvider } from "./providers/ThemeProvider.tsx";
+import { Toaster } from "sonner";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider storageKey="vite-ui-theme">
       <App />
+      <Toaster />
     </ThemeProvider>
   </StrictMode>
 );
