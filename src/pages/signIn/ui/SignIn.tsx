976ac6f27700b76f8527989e7f8e8315f3140a79
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-12-03 21:07:39 +0900

    [add] - Добавлена страница входа и регистрации

---
+++ src/pages/signIn/ui/SignIn.tsx
@@ -0,0 +1,9 @@
+import { SignInForm } from "@/widgets/SignInForm/ui/SignInForm";
+
+export const SignIn = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <SignInForm />
+    </div>
+  );
+};
---
+++ src/pages/signUp/ui/SignUp.tsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from "@/widgets/SignUpForm/ui/SignUpForm";
+
+export const SignUp = () => {
+  return (
+    <div className="flex w-full items-center justify-center">
+      <SignUpForm />
+    </div>
+  );
+};
