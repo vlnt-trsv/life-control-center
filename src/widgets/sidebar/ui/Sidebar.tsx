1ef9a98e36b5ce4f1a4d623018a7d36a8cbb3a85
Valentin Tarasov <111338107+vlnt-trsv@users.noreply.github.com>
2025-12-08 22:11:09 +0700

    [fix]

--- src/widgets/sidebar/ui/Sidebar.tsx
+++ src/widgets/sidebar/ui/Sidebar.tsx
@@ -6,7 +6,6 @@ import {
   SidebarGroupContent,
   SidebarMenu,
   SidebarMenuItem,
-  SidebarMenuButton,
 } from "@/shared/ui/sidebar";
 
 import {
@@ -17,7 +16,7 @@ import {
   ItemMedia,
   ItemTitle,
 } from "@/shared/ui/item";
-import { ChevronsUpDown, LogOut, Settings, User } from "lucide-react";
+import { ChevronsUpDown, LogOut, SettingsIcon, User } from "lucide-react";
 import { UserAvatar } from "@/entities/user/ui/UserAvatar/UserAvatar";
 import {
   DropdownMenu,
@@ -28,64 +27,86 @@ import {
   DropdownMenuTrigger,
 } from "@/shared/ui/dropdown-menu";
 import { useAuthStore } from "@/entities/auth/model/store";
+import { Settings } from "@/widgets/settings/ui/Settings";
+import { Profile } from "@/widgets/profile/ui/Profile";
+import { useUserStore } from "@/entities/user/model/store";
 
 export const Sidebar = () => {
   const [isMenuOpen, setIsMenuOpen] = React.useState(false);
-  const { user, signOut } = useAuthStore();
+  const { signOut } = useAuthStore();
+  const { user } = useUserStore();
+  const [isSettingsOpen, setIsSettingsOpen] = React.useState(false);
+  const [isProfileOpen, setIsProfileOpen] = React.useState(false);
 
   return (
-    <DropdownMenu open={isMenuOpen} onOpenChange={setIsMenuOpen} modal={false}>
-      <SidebarUI variant="floating">
-        <SidebarContent>
-          <SidebarGroup>
-            <SidebarGroupContent>
-              <SidebarMenu>
-                <DropdownMenuTrigger asChild>
-                  <SidebarMenuItem>
-                    <Item
-                      size="sm"
-                      asChild
-                      onClick={() => setIsMenuOpen(isMenuOpen)}
-                      className="cursor-pointer py-1 px-2"
-                    >
-                      <a>
-                        <ItemMedia variant="image">
-                          <UserAvatar />
-                        </ItemMedia>
-                        <ItemContent>
-                          <ItemTitle>{user?.email}</ItemTitle>
-                        </ItemContent>
-                        <ItemActions>
-                          <ChevronsUpDown className="size-4" />
-                        </ItemActions>
-                      </a>
-                    </Item>
-                  </SidebarMenuItem>
-                </DropdownMenuTrigger>
-              </SidebarMenu>
-            </SidebarGroupContent>
-          </SidebarGroup>
-        </SidebarContent>
-      </SidebarUI>
+    <>
+      <DropdownMenu
+        open={isMenuOpen}
+        onOpenChange={setIsMenuOpen}
+        modal={false}
+      >
+        <SidebarUI variant="floating">
+          <SidebarContent>
+            <SidebarGroup>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <DropdownMenuTrigger asChild>
+                    <SidebarMenuItem>
+                      <Item
+                        size="sm"
+                        asChild
+                        onClick={() => setIsMenuOpen(isMenuOpen)}
+                        className="cursor-pointer py-1 px-2"
+                      >
+                        <a>
+                          <ItemMedia variant="image">
+                            <UserAvatar />
+                          </ItemMedia>
+                          <ItemContent>
+                            <ItemTitle>
+                              {user?.user_metadata.username|| "Нет имени пользователя"}
+                            </ItemTitle>
+                            <ItemDescription>
+                              {user?.email || "email@mail.ru"}
+                            </ItemDescription>
+                          </ItemContent>
+                          <ItemActions>
+                            <ChevronsUpDown className="size-4" />
+                          </ItemActions>
+                        </a>
+                      </Item>
+                    </SidebarMenuItem>
+                  </DropdownMenuTrigger>
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </SidebarGroup>
+          </SidebarContent>
+        </SidebarUI>
 
-      <DropdownMenuContent className="w-56" align="start" side="right">
-        <DropdownMenuGroup>
-          <DropdownMenuItem>
-            <User />
-            Профиль
+        <DropdownMenuContent className="w-56" align="start" side="right">
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={() => setIsProfileOpen(!isProfileOpen)}>
+              <User />
+              Профиль
+            </DropdownMenuItem>
+            <DropdownMenuItem
+              onClick={() => setIsSettingsOpen(!isSettingsOpen)}
+            >
+              <SettingsIcon />
+              Настройки
+            </DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem disabled>API</DropdownMenuItem>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem variant="destructive" onClick={() => signOut()}>
+            <LogOut /> Выйти
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <Settings />
-            Настройки
-          </DropdownMenuItem>
-        </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem disabled>API</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem variant="destructive" onClick={() => signOut()}>
-          <LogOut /> Выйти
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <Settings open={isSettingsOpen} onOpenChange={setIsSettingsOpen} />
+      <Profile open={isProfileOpen} onOpenChange={setIsProfileOpen} />
+    </>
   );
 };
